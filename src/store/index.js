edfd5cf03c62e230f8a9dc311b388f45a30991d6
Drew <drewsup123c@gmail.com>
2021-09-23 17:14:30 -0600

    Setup basic redux code

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+//? Import individual reducers here 
+
+const rootReducer =  combineReducers({
+    //? Place combined reducers into this object
+});
+
+export default rootReducer;
